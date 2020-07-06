f9c31bcb1eaa44a654db50f8f9a68ddb8e9048bf
viva0330 <tpgml431@naver.com>
2020-07-06 23:21:33 +0900

    A+B

---
+++ Baekjoon/Bronze/1000.java
@@ -0,0 +1,21 @@
+package replay;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+
+		Scanner s = new Scanner(System.in);
+		int a = 0;
+		int b = 0;
+		a = s.nextInt();
+		b = s.nextInt();
+		System.out.println(a+b);
+		
+	}
+
+}
